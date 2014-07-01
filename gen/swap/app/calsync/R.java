93ee56007c0dc43bb1b4fe889cde185aa0233352
swapnilpatilRajaram <swapnil.patil@students.iiit.ac.in>
2014-07-01 20:55:40 +0530

    Adding a popup

--- gen/swap/app/calsync/R.java
+++ gen/swap/app/calsync/R.java
@@ -32,9 +32,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_add=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int action_add=0x7f090006;
         public static final int action_settings=0x7f090005;
         public static final int dummy_button=0x7f090002;
         public static final int editText1=0x7f090003;
@@ -53,6 +55,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int main_activity_actions=0x7f080002;
     }
     public static final class string {
+        public static final int action_add=0x7f060009;
         public static final int action_settings=0x7f060004;
         public static final int app_name=0x7f060000;
         public static final int button_send=0x7f060007;
--- src/swap/app/calsync/MainActivity.java
+++ src/swap/app/calsync/MainActivity.java
@@ -3,14 +3,30 @@ package swap.app.calsync;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     
+	
+	PopupWindow popUp;
+	LinearLayout layout;
+    TextView tv;
+    LayoutParams params;
+    LinearLayout mainLayout;
+    Button but;
+    boolean click = true;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,52 @@ public class MainActivity extends Activity {
 		startActivity(intent);
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_add:
+	            addBirthday();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 	
+	public void addBirthday()
+	{
+
+
+		
+        popUp = new PopupWindow(this);
+        layout = new LinearLayout(this);
+        mainLayout = new LinearLayout(this);
+        tv = new TextView(this);
+        but = new Button(this);
+        but.setText("Click Me");
+        but.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                if (click) {
+                    popUp.showAtLocation(mainLayout, Gravity.BOTTOM, 10, 10);
+                    popUp.update(50, 50, 300, 80);
+                    click = false;
+                } else {
+                    popUp.dismiss();
+                    click = true;
+                }
+            }
+
+        });
+        params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        tv.setText("Hi this is a sample text for popup window");
+        layout.addView(tv, params);
+        popUp.setContentView(layout);
+        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+        mainLayout.addView(but, params);
+        setContentView(mainLayout);
+	}
 
 }
